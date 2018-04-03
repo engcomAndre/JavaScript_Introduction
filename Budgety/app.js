101545823db385f0b4164d442e3e8bebee27b24b
André Vieira <sgavsnake@gmail.com>
2018-04-03 16:13:00 -0300

    Budgety Add

---
+++ Budgety/app.js
@@ -0,0 +1,8 @@
+var val = document.getElementsByClassName("add__description").item(0).nodeValue;
+
+var btn = document.getElementsByClassName("add__btn").item(0).addEventListener("click",function(){
+    console.log(val);
+    //alert("aaa");
+});
+
+
