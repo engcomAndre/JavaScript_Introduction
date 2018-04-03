6966c718dfc603242d3051c2efdfaec33a8b1d9f
André Vieira <sgavsnake@gmail.com>
2018-04-03 17:27:44 -0300

    Budgety Add In Work

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,8 +1,26 @@
-var val = document.getElementsByClassName("add__description").item(0).nodeValue;
+var description = document.querySelector(".add__description");
+var values = document.querySelector(".add__value");
+var selecctor = document.querySelector(".add__type");
 
-var btn = document.getElementsByClassName("add__btn").item(0).addEventListener("click",function(){
-    console.log(val);
-    //alert("aaa");
+var stringHtml = function(){
+    return  '<div class="item clearfix" id="income-0">'+
+    '<div class="item__description">'+description.value+'</div>'+
+        '<div class="right clearfix">'+
+            '<div class="item__value">'+(selecctor.value=="inc"?" + ":" - ")+' '+ values.value +'</div>'+
+            '<div class="item__delete"></div>'+
+            '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'+
+        '</div>'+''
+    '</div>'+
+    '</div>';
+};
+
+var btn = document.querySelector(".add__btn").addEventListener("click",function(){          
+   
+    document.querySelector(".income__list").innerHTML = stringHtml();
+
+    
 });
+
+
 
 
