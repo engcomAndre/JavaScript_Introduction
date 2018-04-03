78f869881249ac7f867c24e4afa5dbc42a06c479
André Vieira <sgavsnake@gmail.com>
2018-04-03 16:09:28 -0300

    Initial Commit

---
+++ script/Arrays.js
@@ -0,0 +1,17 @@
+console.log("Arrays.js");
+console.log("Arrays in the JavaSacript Pure Language")
+
+console.log("Initilalzing Arrays:")
+var array1 = new Array(1,2,3);
+var array2 = [4,5,6];
+var array3 = Array(7,8,9);
+
+
+console.log("A) var array1 = new Array(1,2,3);");
+console.log("Arrays1 : " + array1);
+
+console.log("B) var array2 = [4,5,6];");
+console.log("Arrays2 : " + array2);
+
+console.log("C) var array3 = Array(7,8,9);");
+console.log("Arrays3 : " + array3);
