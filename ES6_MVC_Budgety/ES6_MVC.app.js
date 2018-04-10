68ffa6c3d1e8cc8e96c6c177ba7e2f680a1b9a33
André Vieira <sgavsnake@gmail.com>
2018-04-10 12:34:47 -0300

    Class MVC JavaScript Init

--- ES6_MVC_Budgety/ES6_MVC.app.js
+++ ES6_MVC_Budgety/ES6_MVC.app.js
@@ -1,49 +1,54 @@
+class Move{
+    constructor(_sign,_description,_value){
+        this.sign = _sign;
+        this.descrition = _description;
+        this.value = _value;
+    }    
+}
 
-let ModelController = (()=>{   
-    
-   
-})();
-
-let ViewController = (()=>{
-
-    let Ds = {
-        inType : ".add__type",
-        inDescription : ".add__description",
-        inValue :".add__value"
-    };
-
-    return{
-        getInput: () =>{
-            return{
-                _type : document.querySelector(Ds.inType).value,
-                _description :document.querySelector(Ds.inDescription).value,
-                _value : document.querySelector(Ds.inValue).value
-            };
-            
-        getDs = () =>{
-                return Ds;
-            };            
-        }
+class ModelController {
+    constructor(){
+        this.Move = new Move; 
+        this.ExpensesList = [];
+        this.IncomeList = [];
     }
-  
-})();
+    addItem(move){
 
-
-
-
-let budgetyController = ((ViewController,ModelController) => { 
-    let controllerAddItem = ()=>{
-
-        (ViewController.readInput());
     }
-    document.querySelector(".add__btn").addEventListener('click',controllerAddItem);
+}
 
-    document.addEventListener('keypress',(event)=>{
-        if(event.key === 13 || event.which === 13){
-            controllerAddItem();
-        }
-    });
+class ViewController {
+    constructor(srcType, srcDescrition, srcValue) {
+        this.Type = "." +srcType;
+        this.Descrition = "." + srcDescrition;
+        this.Value = "." + srcValue;        
+    }
+    getInput(){
+        let _type = document.querySelector(this.Type).value;
+        let _description  = document.querySelector(this.Descrition).value;
+        let _value = document.querySelector(this.Value).value;
+        return new Move(_type,_description,_value);
+    }
+}
 
-        
- 
-})(ViewController,ModelController);
+class BudgetyController {
+    constructor(_ViewController, _ModelController) {
+        this.View = _ViewController;
+        this.Model = _ModelController;
+    } 
+    addMove(){
+        let data  = this.View.getInput();
+        console.log("teste");
+    }   
+}
+
+let View = new ViewController("add__type","add__description","add__value");
+let Model = new ModelController();
+let BdgController = new BudgetyController(View,Model);    
+
+
+var btn = document.querySelector(".add__btn").addEventListener("click", ()=>{
+   console.log(View.getInput());
+});
+
+
