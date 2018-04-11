2d8d7467cca2b8a1f4ed60ee011a86f6bd1178d1
André Vieira <sgavsnake@gmail.com>
2018-04-11 12:44:37 -0300

    UpdateScreen

--- ES6_MVC_Budgety/ES6_MVC.app.js
+++ ES6_MVC_Budgety/ES6_MVC.app.js
@@ -1,33 +1,78 @@
-class Move{
-    constructor(_sign,_description,_value){
+class Moviment {
+    constructor(_sign, _description, _value) {
+        this.id;
         this.sign = _sign;
         this.descrition = _description;
         this.value = _value;
-    }    
+    }
 }
 
+
 class ModelController {
-    constructor(){
-        this.Move = new Move; 
+    constructor() {
+        this.id = 0;
         this.ExpensesList = [];
         this.IncomeList = [];
     }
-    addItem(move){
+    addItem(_Moviment) {
+        _Moviment.id = this.id++;
+        if (_Moviment.sign == "inc") {
+            this.IncomeList.push(_Moviment);
+            return;
+        }
+        this.ExpensesList.push(_Moviment);
+    }
 
+    getMovimentList() {
+        return {
+            Expenses: this.ExpensesList,
+            Incomes: this.IncomeList
+        }
     }
 }
 
 class ViewController {
     constructor(srcType, srcDescrition, srcValue) {
-        this.Type = "." +srcType;
+        this.Type = "." + srcType;
         this.Descrition = "." + srcDescrition;
-        this.Value = "." + srcValue;        
+        this.Value = "." + srcValue;
     }
-    getInput(){
+    getInput() {
         let _type = document.querySelector(this.Type).value;
-        let _description  = document.querySelector(this.Descrition).value;
+        let _description = document.querySelector(this.Descrition).value;
         let _value = document.querySelector(this.Value).value;
-        return new Move(_type,_description,_value);
+        return new Moviment(_type, _description, _value);
+    }
+    showData(_Moviment) {
+        let type = _Moviment.descrition == "inc" ? "Income-" : "Expense-";
+        let percent = "NI";//not implemented
+        return
+        `<div class="item clearfix" id=${type}${_Moviment.id}>
+        <div class="item__description">${_Moviment.descrition}</div>
+        <div class="right clearfix">
+            <div class="item__value">${_Moviment.value}</div>
+            <div class="item__percentage">${percent}</div>
+            <div class="item__delete">
+                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+            </div>
+        </div>
+        </div>`;
+    }
+    showIncomes(_IncomesList) {
+        for (let Inc of _IncomesList) {
+            document.querySelector(".income__list").insertAdjacentHTML("beforeend", this.showData(Inc));
+        }        
+    }
+    showExpenses(_ExpensesList) {
+        for (let Exp of _ExpensesList) {
+            document.querySelector(".expenses__list").insertAdjacentHTML("beforeend", this.showData(Exp));
+        }    
+    }
+    showAllMoviment(_Moviment){
+        document.querySelector(".income__list").innerHTML = "";
+        document.querySelector(".expenses__list").innerHTML = "";  
+        this.showIncomes(_Moviment.Incomes);
+        this.showExpenses(_Moviment.Expenses);
     }
 }
 
@@ -35,20 +80,27 @@ class BudgetyController {
     constructor(_ViewController, _ModelController) {
         this.View = _ViewController;
         this.Model = _ModelController;
-    } 
-    addMove(){
-        let data  = this.View.getInput();
-        console.log("teste");
-    }   
+
+    }
+    addMove() {
+        let data = this.View.getInput();
+        this.Model.addItem(data);
+        console.log(this.Model.getMovimentList());
+    }
+    updateScreen(){
+        let Moviment = Model.getMovimentList();       
+        View.showAllMoviment(Moviment);        
+    }
 }
 
-let View = new ViewController("add__type","add__description","add__value");
+let View = new ViewController("add__type", "add__description", "add__value");
 let Model = new ModelController();
-let BdgController = new BudgetyController(View,Model);    
+let BdgController = new BudgetyController(View, Model);
 
 
-var btn = document.querySelector(".add__btn").addEventListener("click", ()=>{
-   console.log(View.getInput());
+var btn = document.querySelector(".add__btn").addEventListener("click", () => {
+    BdgController.addMove();
+    BdgController.updateScreen();
 });
 
 
