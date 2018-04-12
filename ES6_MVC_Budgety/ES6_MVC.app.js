113286938d2f143a4af18905194304137c58f463
André Vieira da Silva <sgavsnake@gmail.com>
2018-04-12 00:22:28 -0300

    Delete Btn

--- ES6_MVC_Budgety/ES6_MVC.app.js
+++ ES6_MVC_Budgety/ES6_MVC.app.js
@@ -4,15 +4,35 @@ class Moviment {
         this.sign = _sign;
         this.descrition = _description;
         this.value = _value;
+        if (this.sign === "exp") {
+            this.Percent = "---";
+        }
     }
 }
 
+
+class Math {
+
+    static calcTotal(list) {
+        let sum = 0;
+        for (let i = 0; i < list.length; i++) {
+            sum += parseInt(list[i].value);
+        }
+        return sum;
+    }   
+
+    static calcPercenValues(ReferenceValue, ReferenceTotal) {
+        return `${parseInt(100 * (ReferenceValue / ReferenceTotal))}%`;
+    }
+}
 
 class ModelController {
     constructor() {
         this.id = 0;
         this.ExpensesList = [];
         this.IncomeList = [];
+        this.ExpenseTotal = 0;
+        this.IncomeTotal = 0;                
     }
     addItem(_Moviment) {
         _Moviment.id = this.id++;
@@ -22,10 +42,24 @@ class ModelController {
         }
         this.ExpensesList.push(_Moviment);
     }
-    getMovimentList() {
+    updatePercents() {        
+        for (let i = 0; i < this.ExpensesList.length; i++) {            
+            this.ExpensesList[i].Percent = Math.calcPercenValues(this.ExpensesList[i].value, this.IncomeTotal)
+        }        
+    }    
+
+    updateValues() {
+        this.ExpenseTotal = Math.calcTotal(this.ExpensesList);
+        this.IncomeTotal = Math.calcTotal(this.IncomeList);
+        this.updatePercents();        
+    }
+
+    getMovimentData() {
         return {
+            ExpenseTotal: this.ExpenseTotal,
+            IncomeTotal: this.IncomeTotal,
             Expenses: this.ExpensesList,
-            Incomes: this.IncomeList
+            Incomes: this.IncomeList,
         }
     }
 }
@@ -42,38 +76,88 @@ class ViewController {
         let _value = document.querySelector(this.Value).value;
         return new Moviment(_type, _description, _value);
     }
-    showData(_Moviment) {             
-        let type = _Moviment.descrition == "inc" ? "Income-" : "Expense-";
-        let percent = "NI";//not implemented
-        let html =  `<div class="item clearfix" id=${type}${_Moviment.id}>
-        <div class="item__description">${_Moviment.descrition}</div>
-        <div class="right clearfix">
-            <div class="item__value">${_Moviment.value}</div>
-            <div class="item__percentage">${percent}</div>
-            <div class="item__delete">
+
+    showData(_Moviment) {
+        let type = "- Expense-";
+
+        if (_Moviment.sign === "inc") {
+            type = "+ Income-";
+            return `<div class="item clearfix" id=${type}${_Moviment.id}>
+            <div class="item__description">${_Moviment.descrition}</div>
+            <div class="right clearfix">
+                <div class="item__value">+ ${_Moviment.value}</div>                
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+            </div>`;
+        }
+        return `<div class="item clearfix" id=${type}${_Moviment.id}>
+                <div class="item__description">${_Moviment.descrition}</div>
+                <div class="right clearfix">
+                <div class="item__value">- ${_Moviment.value}</div>
+                <div class="item__percentage">${_Moviment.Percent}</div>
+                <div class="item__delete">
                 <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
             </div>
         </div>
-        </div>`;       
-
-        return html;
+        </div>`;
     }
     showIncomes(_IncomesList) {
         for (let Inc of _IncomesList) {
             document.querySelector(".income__list").insertAdjacentHTML("beforeend", this.showData(Inc));
-        }        
+        }
     }
     showExpenses(_ExpensesList) {
         for (let Exp of _ExpensesList) {
             document.querySelector(".expenses__list").insertAdjacentHTML("beforeend", this.showData(Exp));
-        }    
+        }
+    }    
+    showTotalPercent(_Moviment){
+        let totalPercet = 0;
+        if(_Moviment.IncomeTotal > 0){            
+            totalPercet = Math.calcPercenValues(_Moviment.ExpenseTotal,_Moviment.IncomeTotal);
+            document.querySelector(".budget__expenses--percentage").innerHTML = `${totalPercet}%`;
+            return;
+        }
+        
+        console.log(totalPercet);
+        document.querySelector(".budget__expenses--percentage").innerHTML = "---";
     }
-    showAllMoviment(_Moviment){
+
+    clearScreen(){
         document.querySelector(".income__list").innerHTML = "";
-        document.querySelector(".expenses__list").innerHTML = "";  
+        document.querySelector(".expenses__list").innerHTML = "";
+        document.querySelector(".budget__expenses--percentage").innerHTML = "---";
+        document.querySelector(".budget__value").innerHTML = "0";
+        
+    }
+    showTotals(_Moviment){
+        document.querySelector(".budget__income--value").innerHTML = `+ ${_Moviment.IncomeTotal}`;
+        document.querySelector(".budget__expenses--value").innerHTML = `- ${_Moviment.ExpenseTotal}`;
+        
+        
+        let percent = Math.calcPercenValues(_Moviment.ExpenseTotal,_Moviment.IncomeTotal);
+        //document.querySelector(".budget__expenses--percentage").innerHTML = `${percent}`;
+
+
+        let resBudgety = _Moviment.IncomeTotal - _Moviment.ExpenseTotal;        
+        if(resBudgety > 0){
+            document.querySelector(".budget__value").innerHTML = `+ ${resBudgety}`;
+            return;
+        }
+        if(resBudgety < 0){
+            document.querySelector(".budget__value").innerHTML = `- ${resBudgety}`;
+        }                        
+        
+    }
+
+    showAllMoviments(_Moviment) {                
+        this.clearScreen();        
+        this.showTotals(_Moviment);
         this.showIncomes(_Moviment.Incomes);
         this.showExpenses(_Moviment.Expenses);
-        console.log("controlle - "+_Moviment);
+        this.showTotalPercent(_Moviment);
     }
 }
 
@@ -85,13 +169,12 @@ class BudgetyController {
     addMove() {
         let data = this.View.getInput();
         this.Model.addItem(data);
-        console.log(this.Model.getMovimentList());
+        this.Model.updateValues();
     }
-    updateScreen(){
-        console.log("controller");
-        let Moviment = Model.getMovimentList();       
-        View.showAllMoviment(Moviment);        
-        console.log("controller");
+
+    updateScreen() {
+        let Moviment = Model.getMovimentData();        
+        View.showAllMoviments(Moviment);
     }
 }
 
@@ -99,17 +182,20 @@ let View = new ViewController("add__type", "add__description", "add__value");
 let Model = new ModelController();
 let BdgController = new BudgetyController(View, Model);
 
+BdgController.updateScreen();
+
 var btn = document.querySelector(".add__btn").addEventListener("click", () => {
     BdgController.addMove();
     BdgController.updateScreen();
 });
 
 var enter = document.addEventListener("keypress", (event) => {
-    if(event.key ===13 || event.which ===13){
+    if (event.key === 13 || event.which === 13) {
         BdgController.addMove();
         BdgController.updateScreen();
-    }    
+    }
 });
+
 
 
 
